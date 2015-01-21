bf4c9b192ee8d59047948b3ce5912141316c7d79
frostic3 <frostic3@users.noreply.github.com>
2015-01-21 17:25:10 +0530

    Add dotfile bash_profile & Update file list in makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="gitconfig vimrc.before.local vimrc.bundles.local vimrc.local"    # list of files/folders to symlink in homedir
+files="bash_profile gitconfig vimrc.before.local vimrc.bundles.local vimrc.local"    # list of files/folders to symlink in homedir
 
 ##########
 
